5be943ed4769473ff896152577e4d3ca8baba60c
Sanskar <sanskarsrs9@gmail.com>
2025-04-28 02:09:31 +0530

    first commit

---
+++ pointers/pointer.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a = 10;
+
+int *z = &a;
+
+cout<<z<<endl;
+
+int *p = &z;
+
+cout<<p<<endl;
+
+    cout << a << endl;
+    return 0;
+}
